202b1a1f59aa49c8e88583493f3d30ddde495943
viadsten <stanulevich.vg@yandex.ru>
2024-01-13 15:09:48 +0300

    Chore: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/components/home/home.component';
+import { AuthComponent } from './features/components/auth/auth.component';
+import { ProfileComponent } from './features/components/profile/profile.component';
+import { FeedListComponent } from './features/components/feed-list/feed-list.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: FeedListComponent
+  }, {
+    path: 'sign-in',
+    component: AuthComponent
+  }, {
+    path: 'sign-up',
+    component: AuthComponent
+  }, {
+    path: 'profile',
+    component: ProfileComponent
+  }, {
+    path: 'favorites',
+    component: FeedListComponent
+  }
+];
