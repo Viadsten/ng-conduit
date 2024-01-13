2f8f243ea51053f2959116cfe4852242622c3477
viadsten <stanulevich.vg@yandex.ru>
2024-01-13 15:10:05 +0300

    Chore: create app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { InputComponent } from './ui/input/input.component';
+import { ButtonComponent } from './ui/button/button.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InputComponent, ButtonComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'conduit';
 }
